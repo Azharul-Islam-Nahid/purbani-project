85cce2bb0ee864393546cc96d8e11f824a81ba48
Azharul Islam <lonewolfazharul@gmail.com>
2023-06-18 23:17:54 +0600

    buyer logo and text

--- pages/dashboard/buyer/index.js
+++ pages/dashboard/buyer/index.js
@@ -1,18 +1,28 @@
 
-import { HiOutlineBuildingOffice } from "react-icons/hi2";
+import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
 import DashboardLayout from "../../../components/common/DashboardLayout";
 import styles from '../../../styles/upload.module.css'
+import { MdPeople } from "react-icons/md";
 
 
 export default function Home() {
     return (
         <DashboardLayout title="buyer">
             <div className={`${styles.uploadCon} h-2/4 w-2/4`}>
-                <div>
-                    <p className="text-white text-7xl">{HiOutlineBuildingOffice}</p>
+                <div className="mt-16 flex justify-evenly text-color_pink">
+                    <div>
+                        <p className="text-8xl"><HiOutlineBuildingOffice2 />
+                        </p>
+                        <p className="text-justify font-extrabold">COMPANIES</p>
+                    </div>
+                    <div>
+                        <p className="text-8xl"><MdPeople />
+                        </p>
+                        <p className="text-center font-extrabold">BUYER</p>
+                    </div>
                 </div>
             </div>
 
-        </DashboardLayout>
+        </DashboardLayout >
     );
 }
