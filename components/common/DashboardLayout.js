0a12ecb4c94e005de3244e00d6afc32ad992e0e2
Ruman-Islam <rumanislam48@gmail.com>
2023-06-18 17:38:52 +0600

    header done

--- components/common/DashboardLayout.js
+++ components/common/DashboardLayout.js
@@ -2,6 +2,11 @@ import { useState, useEffect, useContext } from "react";
 import Sidebar from "./Sidebar";
 import { authContext } from "../../context/authContext";
 import { useRouter } from "next/router";
+import styles from "../../styles/Sidebar.module.css";
+import Image from "next/image";
+import logoPurbani from "../../public/assets/Logos/Logo_Purbani.png";
+import profileImg from "../../public/assets/images/ruman2.jpg";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 const DashboardLayout = ({ children }) => {
   const { state, dispatch } = useContext(authContext);
@@ -15,9 +20,38 @@ const DashboardLayout = ({ children }) => {
     <>
       <div className="flex justify-between">
         <Sidebar />
-        <div className="flex-1">
-          <div>header</div>
-          <div className="flex flex-col justify-center items-center">
+        <div className="flex-1 pr-5">
+          <div className={`${styles.dashboardHeader}`}>
+            <div className="">
+              <Image
+                className="rounded-full"
+                src={logoPurbani}
+                width={200}
+                height={50}
+                alt=""
+              />
+            </div>
+            <div className="text-white">
+              <div
+                className={`${styles.profileBtn} flex justify-center items-center gap-x-3 p-3`}
+              >
+                <div className="w-[30px] h-[30px] rounded-full border">
+                  <Image
+                    className="rounded-full"
+                    src={profileImg}
+                    width={100}
+                    height={100}
+                    alt=""
+                  />
+                </div>
+                <span>Ruman Islam</span>
+                <button>
+                  <IoIosArrowDropdownCircle className="text-2xl" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center ">
             {children}
           </div>
         </div>
