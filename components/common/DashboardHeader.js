b0c8af2ebbeca7bc02c1998df5d68d2a540c66ef
Ruman-Islam <rumanislam48@gmail.com>
2023-06-20 14:46:28 +0600

    background image issue fixed

--- components/common/DashboardHeader.js
+++ components/common/DashboardHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../../styles/Sidebar.module.css";
 import Image from "next/image";
-import logoPurbani from "../../public/assets/Logos/Logo_Purbani 4.png";
+import logoPurbani from "../../public/assets/Logos/logo-purbani.png";
 import profileImg from "../../public/assets/images/ruman2.jpg";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 
@@ -30,7 +30,7 @@ const DashboardHeader = () => {
               alt=""
             />
           </div>
-          <span>Ruman Islam</span>
+          <span>Admin 1</span>
           <button>
             <IoIosArrowDropdownCircle className="text-2xl" />
           </button>
--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { AuthProvider } from "../context/authContext.js";
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <div className="bg-[url('https://i.ibb.co/8Ycnyqx/bg-primary.png')] bg-no-repeat bg-cover">
+      <div className="bg-[url('https://i.ibb.co/8Ycnyqx/bg-primary.png')] bg-no-repeat bg-cover h-screen">
         <Component {...pageProps} />
       </div>
     </AuthProvider>
