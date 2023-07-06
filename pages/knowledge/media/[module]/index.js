671a671f3d0d700a97baf6ed8bdf6f0a7e3dd148
Azharul Islam <lonewolfazharul@gmail.com>
2023-07-06 15:03:13 +0600

    dynamic page in knowledge

--- pages/knowledge/media/[module]/index.js
+++ pages/knowledge/media/[module]/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
+
 const index = () => {
     return (
-        <div>
-            <h1>here all module</h1>
+        <div className='m-auto pt-20'>
+            <div className='m-auto bg-white w-8/12 h-3/6  overflow-y-auto'>
+                <div className='ml-40 flex flex-col'>
+                    <div>pdf shows here</div>
+                    <div>
+                        <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
+                    </div>
+                </div>
+                <div className='ml-40 flex flex-col'>
+                    <div>pdf shows here</div>
+                    <div>
+                        <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
+                    </div>
+                </div>
+                <div className='ml-40 flex flex-col'>
+                    <div>pdf shows here</div>
+                    <div>
+                        <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
