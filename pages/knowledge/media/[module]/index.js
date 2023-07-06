cf6eec4100464c7cda366047638c40e85eff7fc3
Azharul Islam <lonewolfazharul@gmail.com>
2023-07-06 22:59:52 +0600

    knowledge page design fix

--- pages/knowledge/media/[module]/index.js
+++ pages/knowledge/media/[module]/index.js
@@ -1,30 +1,88 @@
 import React from 'react';
+import Layout from '../../../../components/common/Layout';
+import Navbar from '../../../../components/common/navbar';
+import { BsEye } from 'react-icons/bs';
 
 const index = () => {
     return (
-        <div className='m-auto pt-20'>
-            <div className='border rounded-md m-auto bg-white w-8/12 h-96
+
+        <Layout>
+            <Navbar />
+            <div className='m-auto pt-20'>
+                <div className='border rounded-md m-auto bg-white w-8/12 h-96
              overflow-y-scroll'>
-                <div className='ml-40 flex flex-col'>
-                    <div>pdf shows here</div>
-                    <div>
-                        <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
+                    <div className='mt-5 mb-5 mr-20 ml-40 flex flex-col'>
+                        <div className="flex justify-between flex-row">
+                            <div className="text-lg font-bold px-4 flex gap-x-1 items-center">
+                                The pdf file name goes right here
+                                {/* {idx + 1}. {item.title} */}
+
+                            </div>
+                            <div className="flex gap-x-5">
+                                <a
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    // href={item?.readableLink}
+                                    className="cursor-pointer text-2xl text-color_brand hover:text-color_dark transition-all duration-200"
+                                >
+                                    <BsEye />
+                                </a>
+
+                            </div>
+                        </div>
+                        <div>
+                            <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
+                        </div>
                     </div>
-                </div>
-                <div className='ml-40 flex flex-col'>
-                    <div>pdf shows here</div>
-                    <div>
-                        <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
+                    <div className='mb-5 mr-20 ml-40 flex flex-col'>
+                        <div className="flex justify-between flex-row">
+                            <div className="text-lg font-bold px-4 flex gap-x-1 items-center">
+                                The pdf file name goes right here
+                                {/* {idx + 1}. {item.title} */}
+
+                            </div>
+                            <div className="flex gap-x-5">
+                                <a
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    // href={item?.readableLink}
+                                    className="cursor-pointer text-2xl text-color_brand hover:text-color_dark transition-all duration-200"
+                                >
+                                    <BsEye />
+                                </a>
+
+                            </div>
+                        </div>
+                        <div>
+                            <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
+                        </div>
                     </div>
-                </div>
-                <div className='ml-40 flex flex-col'>
-                    <div>pdf shows here</div>
-                    <div>
-                        <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
+                    <div className='mb-5 mr-20 ml-40 flex flex-col'>
+                        <div className="flex justify-between flex-row">
+                            <div className="text-lg font-bold px-4 flex gap-x-1 items-center">
+                                The pdf file name goes right here
+                                {/* {idx + 1}. {item.title} */}
+
+                            </div>
+                            <div className="flex gap-x-5">
+                                <a
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    // href={item?.readableLink}
+                                    className="cursor-pointer text-2xl text-color_brand hover:text-color_dark transition-all duration-200"
+                                >
+                                    <BsEye />
+                                </a>
+
+                            </div>
+                        </div>
+                        <div>
+                            <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Layout>
     );
 };
 
