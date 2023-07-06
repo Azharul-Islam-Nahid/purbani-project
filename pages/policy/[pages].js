dc2a205a22a33cfd84d09ef4d180cf27d725e9e9
Ruman-Islam <rumanislam48@gmail.com>
2023-07-06 14:59:05 +0600

    color change

--- pages/policy/[pages].js
+++ pages/policy/[pages].js
@@ -83,6 +83,7 @@ const PageDetails = () => {
                         <FaDownload />
                       </a>
                     </div>
+                    
                   </div>
                 );
               })}
