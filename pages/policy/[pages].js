456dbb5c8452ecd1c2ab1286b8fbffb4c26b0150
Azharul Islam <lonewolfazharul@gmail.com>
2023-07-06 23:01:17 +0600

    fix

--- pages/policy/[pages].js
+++ pages/policy/[pages].js
@@ -1,13 +1,13 @@
-import { FaDownload } from "react-icons/fa";
-import { AiFillFilePdf } from "react-icons/ai";
-import { BsEye } from "react-icons/bs";
-import Layout from "../../components/common/Layout";
-import Navbar from "../../components/common/navbar";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { baseUrl, getHeaders } from "../../api/api";
-import { useSession } from "next-auth/react";
-import axios from "axios";
+import { FaDownload } from 'react-icons/fa';
+import { AiFillFilePdf } from 'react-icons/ai';
+import { BsEye } from 'react-icons/bs';
+import Layout from '../../components/common/Layout';
+import Navbar from '../../components/common/navbar';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { baseUrl, getHeaders } from '../../api/api';
+import { useSession } from 'next-auth/react';
+import axios from 'axios';
 
 const PageDetails = () => {
   const { data: state } = useSession();
@@ -18,13 +18,13 @@ const PageDetails = () => {
 
   let url = `/policy/get-all-policy?department`;
   if (
-    pages === "certification" ||
-    pages === "buyer" ||
-    pages === "forms" ||
-    pages === "agreements"
+    pages === 'certification' ||
+    pages === 'buyer' ||
+    pages === 'forms' ||
+    pages === 'agreements'
   ) {
     url = `/policy/get-all-policy?subDepartment`;
-  } else if (pages === "law") {
+  } else if (pages === 'law') {
     url = `/policy/get-all-policy?searchTerm`;
   }
 
@@ -45,10 +45,10 @@ const PageDetails = () => {
 
   if (loading) {
     return (
-      <Layout title="Loading">
-        <div className="w-full h-screen flex flex-col justify-center items-center">
-          <div className="flex justify-center relative">
-            <div className="custom-loader"></div>
+      <Layout title='Loading'>
+        <div className='w-full h-screen flex flex-col justify-center items-center'>
+          <div className='flex justify-center relative'>
+            <div className='custom-loader'></div>
           </div>
         </div>
       </Layout>
@@ -57,29 +57,29 @@ const PageDetails = () => {
 
   return (
     <Layout title={pages}>
-      <div className="h-screen overflow-y-auto">
+      <div className='h-screen overflow-y-auto'>
         <Navbar />
         {policy?.length && (
-          <div className="w-full flex items-center justify-center pt-28">
-            <div className="grid  grid-cols-1 bg-white rounded-lg w-[800px] py-20 px-10 h-full">
+          <div className='w-full flex items-center justify-center pt-28'>
+            <div className='grid  grid-cols-1 bg-white rounded-lg w-[800px] py-20 px-10 h-full'>
               {policy?.map((item, idx) => {
                 return (
-                  <div key={idx} className="flex justify-between flex-row mb-2">
-                    <div className="text-lg font-bold px-4 flex gap-x-1 items-center">
+                  <div key={idx} className='flex justify-between flex-row mb-2'>
+                    <div className='text-lg font-bold px-4 flex gap-x-1 items-center'>
                       {idx + 1}. {item.title}
-                      <AiFillFilePdf className="text-color_brand" />
+                      <AiFillFilePdf className='text-color_brand' />
                     </div>
-                    <div className="flex gap-x-5">
+                    <div className='flex gap-x-5'>
                       <a
-                        target="_blank"
-                        rel="noreferrer"
+                        target='_blank'
+                        rel='noreferrer'
                         href={item?.readableLink}
-                        className="cursor-pointer text-2xl text-color_brand hover:text-color_dark transition-all duration-200"
+                        className='cursor-pointer text-2xl text-color_brand hover:text-color_dark transition-all duration-200'
                       >
                         <BsEye />
                       </a>
                       <a
-                        className="text-2xl text-color_brand hover:text-color_dark transition-all duration-200"
+                        className='text-2xl text-color_brand hover:text-color_dark transition-all duration-200'
                         href={item?.downloadableLink}
                       >
                         <FaDownload />
@@ -91,7 +91,7 @@ const PageDetails = () => {
             </div>
           </div>
         )}
-        <div className="text-white text-center pt-6 text-xl capitalize">
+        <div className='text-white text-center pt-6 text-xl capitalize'>
           {`${state?.user?.name}, Welcome to Purbani Document Management System`}
         </div>
       </div>
