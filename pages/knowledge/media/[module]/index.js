01a750c27e23b2d50e6f8f4564123bf23e8f2756
Azharul Islam <lonewolfazharul@gmail.com>
2023-07-07 00:03:39 +0600

    code scaling

--- pages/knowledge/media/[module]/index.js
+++ pages/knowledge/media/[module]/index.js
@@ -1,89 +1,60 @@
-import React from 'react';
-import Layout from '../../../../components/common/Layout';
-import Navbar from '../../../../components/common/navbar';
-import { BsEye } from 'react-icons/bs';
+import Layout from "../../../../components/common/Layout";
+import Navbar from "../../../../components/common/navbar";
+import { BsEye } from "react-icons/bs";
 
-const index = () => {
+const Index = () => {
+    const data = [
+        {
+            title: "The pdf file name goes right here",
+            videoUrl: "https://youtu.be/1nFLGjg9RxU",
+        },
+        {
+            title: "The pdf file name goes right here",
+            videoUrl: "https://youtu.be/1nFLGjg9RxU",
+        },
+        {
+            title: "The pdf file name goes right here",
+            videoUrl: "https://youtu.be/1nFLGjg9RxU",
+        },
+    ];
+
     return (
-
-        <Layout>
-            <Navbar />
-            <div className='m-auto pt-20'>
-                <div className='border rounded-md m-auto bg-white w-8/12 h-96
-             overflow-y-scroll'>
-                    <div className='mt-5 mb-5 mr-20 ml-40 flex flex-col'>
-                        <div className="flex justify-between flex-row">
-                            <div className="text-lg font-bold px-4 flex gap-x-1 items-center">
-                                The pdf file name goes right here
-                                {/* {idx + 1}. {item.title} */}
-
+        <>
+            <Layout>
+                <Navbar />
+                <div className="m-auto pt-20">
+                    <div className="border rounded-md m-auto bg-white w-8/12 h-96 overflow-y-scroll">
+                        {data.map((item, idx) => (
+                            <div key={idx} className="mt-5 mb-5 mr-20 ml-40 flex flex-col">
+                                <div className="flex justify-between flex-row">
+                                    <div className="text-lg font-bold px-4 flex gap-x-1 items-center">
+                                        {item.title}
+                                    </div>
+                                    <div className="flex gap-x-5">
+                                        <a
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            // href={item?.readableLink}
+                                            className="cursor-pointer text-2xl text-color_brand hover:text-color_dark transition-all duration-200"
+                                        >
+                                            <BsEye />
+                                        </a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <video
+                                        controls
+                                        src={item.videoUrl}
+                                        style={{ width: "80%" }}
+                                    />
+                                </div>
                             </div>
-                            <div className="flex gap-x-5">
-                                <a
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    // href={item?.readableLink}
-                                    className="cursor-pointer text-2xl text-color_brand hover:text-color_dark transition-all duration-200"
-                                >
-                                    <BsEye />
-                                </a>
-
-                            </div>
-                        </div>
-                        <div>
-                            <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
-                        </div>
-                    </div>
-                    <div className='mb-5 mr-20 ml-40 flex flex-col'>
-                        <div className="flex justify-between flex-row">
-                            <div className="text-lg font-bold px-4 flex gap-x-1 items-center">
-                                The pdf file name goes right here
-                                {/* {idx + 1}. {item.title} */}
-
-                            </div>
-                            <div className="flex gap-x-5">
-                                <a
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    // href={item?.readableLink}
-                                    className="cursor-pointer text-2xl text-color_brand hover:text-color_dark transition-all duration-200"
-                                >
-                                    <BsEye />
-                                </a>
-
-                            </div>
-                        </div>
-                        <div>
-                            <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
-                        </div>
-                    </div>
-                    <div className='mb-5 mr-20 ml-40 flex flex-col'>
-                        <div className="flex justify-between flex-row">
-                            <div className="text-lg font-bold px-4 flex gap-x-1 items-center">
-                                The pdf file name goes right here
-                                {/* {idx + 1}. {item.title} */}
-
-                            </div>
-                            <div className="flex gap-x-5">
-                                <a
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    // href={item?.readableLink}
-                                    className="cursor-pointer text-2xl text-color_brand hover:text-color_dark transition-all duration-200"
-                                >
-                                    <BsEye />
-                                </a>
-
-                            </div>
-                        </div>
-                        <div>
-                            <video controls src='https://youtu.be/1nFLGjg9RxU' style={{ width: "80%" }} />
-                        </div>
+                        ))}
                     </div>
                 </div>
-            </div>
-        </Layout>
+            </Layout>
+        </>
     );
 };
 
-export default index;
+export default Index;
