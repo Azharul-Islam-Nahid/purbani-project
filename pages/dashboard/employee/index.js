2acddd97ad615771fbfc997f825b3d84bc56ca3f
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-07-07 17:26:01 +0600

    change user list

--- pages/dashboard/employee/index.js
+++ pages/dashboard/employee/index.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import DashboardLayout from '../../../components/common/DashboardLayout';
 import Image from 'next/image';
 import { FaUser } from 'react-icons/fa';
-
+import { AiFillEdit, AiOutlineDelete } from 'react-icons/ai'
+// import {AiOutlineDelete} from 'react-icons/ai'
 const users = [
     {
         name: "ABU Hanif Chowdhuri",
@@ -63,6 +64,15 @@ const users = [
 ]
 const Employeers = () => {
     const [status, setStatus] = useState("loading");
+    // http://localhost:5000/api/v1/users/get-all-user
+
+    async function fetchData() {
+        const res = await fetch('http://localhost:5000/api/v1/users/get-all-user'); // Replace with your API endpoint
+        const data = await res.json();
+        return data;
+      }
+      
+    const data = fetchData()
     useEffect(() => {
         // Simulating an asynchronous process
         setTimeout(() => {
@@ -84,7 +94,7 @@ const Employeers = () => {
     return (
         <DashboardLayout>
             <div className='mt-5'>
-                <table className='bg-dark_overlay min-w-[700px] text-center p-6'>
+                <table className='bg-dark_overlay min-w-[800px] text-center p-6'>
                     {/* head */}
                     <thead>
                         <tr>
@@ -92,7 +102,7 @@ const Employeers = () => {
                             <th>Name</th>
                             <th>Role</th>
                             <th>Action</th>
-                            <th>Knowledge</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -101,8 +111,14 @@ const Employeers = () => {
                                 <th className='p-2'>{index + 1}</th>
                                 <td className='p-2'>{user.name}</td>
                                 <td className='p-2'>{user.role}</td>
-                                <td className='cursor-pointer p-2'>Delete</td>
-                                <td className='cursor-pointer p-2'>Access</td>
+                                <td className='cursor-pointer p-2 '><div className='flex items-center gap-x-2'>
+                                    <AiFillEdit /> <span>Edit</span>
+                                </div></td>
+                                <td className='cursor-pointer p-2 hover:text-red-500'>
+                                    <div className='flex items-center gap-x-2'>
+                                    <AiOutlineDelete /> <span>Delete</span>
+                                    </div>
+                                </td>
                             </tr>)
                         }
 
