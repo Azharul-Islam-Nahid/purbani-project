4163a1604e2282bf9ef6c6381e1cb1f8c3e6b443
Ruman-Islam <rumanislam48@gmail.com>
2023-07-07 17:25:05 +0600

    fixed

--- pages/knowledge/index.js
+++ pages/knowledge/index.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import styles from '../../styles/knowledge.module.css'
-import Layout from '../../components/common/Layout';
-import Navbar from '../../components/common/navbar';
-import { useRouter } from 'next/router';
+import React from "react";
+import styles from "../../styles/knowledge.module.css";
+import Layout from "../../components/common/Layout";
+import Navbar from "../../components/common/navbar";
+import { useRouter } from "next/router";
 const Knowledge = () => {
-    const router = useRouter()
-    return (
-        <Layout title={"Knowledge"}>
-            <div className='flex flex-col  items-center h-screen overflow-y-auto'>
-                <Navbar />
-                <div className='w-full flex items-center justify-center mt-10'>
-                    <div
-                        onClick={() => router.push("/knowledge/media")}
-                        className={`${styles.optionCard} max-w-[279px] h-[266px] w-full p-[20px] relative group hover:bg-color_brand duration-300 cursor-pointer`}
-                    >
-                        <div className="text-5xl absolute text-color_pink group-hover:text-color_white duration-300">
-                            1
-                        </div>
-                        <div className="text-xl font-semibold h-full flex justify-center items-center">
-                            SAP
-                        </div>
-                    </div>
-                </div>
+  const router = useRouter();
+  return (
+    <Layout title={"Knowledge"}>
+      <div className="flex flex-col  items-center h-screen overflow-y-auto">
+        <Navbar />
+        <div className="w-full flex items-center justify-center mt-10">
+          <div
+            onClick={() => router.push("/knowledge/media")}
+            className={`${styles.optionCard} max-w-[279px] h-[266px] w-full p-[20px] relative group hover:bg-color_brand duration-300 cursor-pointer`}
+          >
+            <div className="text-5xl absolute text-color_pink group-hover:text-color_white duration-300">
+              1
             </div>
-
-        </Layout>
-
-    );
+            <div className="text-xl font-semibold h-full flex justify-center items-center">
+              SAP
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
-Knowledge.auth = true
+Knowledge.auth = true;
 export default Knowledge;
