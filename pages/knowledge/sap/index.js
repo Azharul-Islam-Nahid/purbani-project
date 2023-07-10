d1a5a2fc12c9da61ed54b173933312d920e63849
Ruman-Islam <rumanislam48@gmail.com>
2023-07-10 22:16:09 +0600

    pull

--- pages/knowledge/sap/index.js
+++ pages/knowledge/sap/index.js
@@ -1,68 +1,77 @@
-import router, { useRouter } from 'next/router';
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
-import styles from '../../../styles/knowledge.module.css'
-import { useEffect, useState } from 'react';
+import router, { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import styles from "../../../styles/knowledge.module.css";
+import { useEffect, useState } from "react";
 
 // Dynamically import components
-const Layout = dynamic(() => import('../../../components/common/Layout'));
-const Navbar = dynamic(() => import('../../../components/common/navbar'));
+const Layout = dynamic(() => import("../../../components/common/Layout"));
+const Navbar = dynamic(() => import("../../../components/common/navbar"));
 
 // Reusable OptionCard component
-const OptionCard = ({ number, title }) => {
-
-    const [user, setUser] = useState({});
-    const router = useRouter();
-
-
-    console.log("🚀 ~ file: index.js:17 ~ OptionCard ~ user:", user)
-
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("user"));
-        setUser(user);
-    }, []);
-
-
-    console.log(user?.role);
-
-    // if (user?.role === 'super_admin') {
+const OptionCard = ({ number, title, user }) => {
+  if (user?.role === "super_admin") {
+    
     return (
-        <Link href={`sap/${title.toLowerCase()}`}>
-            <a className={`${styles.optionCard} max-w-[279px] h-[266px] w-full p-[20px] relative group hover:bg-color_brand duration-300 cursor-pointer`}>
-                <div className="text-5xl absolute text-color_pink group-hover:text-color_white duration-300">
-                    {number}
-                </div>
-                <div className="text-xl font-semibold h-full flex justify-center items-center">
-                    {title}
-                </div>
-            </a>
-        </Link>
+      <Link href={`sap/${title.toLowerCase()}`}>
+        <a
+          className={`${styles.optionCard} max-w-[279px] h-[266px] w-full p-[20px] relative group hover:bg-color_brand duration-300 cursor-pointer`}
+        >
+          <div className="text-5xl absolute text-color_pink group-hover:text-color_white duration-300">
+            {number}
+          </div>
+          <div className="text-xl font-semibold h-full flex justify-center items-center">
+            {title}
+          </div>
+        </a>
+      </Link>
     );
-    // }
-
-    // else {
-    //     router.push('/unauthorized')
-    // }
-}
+  } else {
+    router.push("/unauthorized");
+  }
+};
 
 const KnowledgeMedia = () => {
+  const router = useRouter();
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    setUser(user);
+    setLoading(false);
+  }, []);
+
+  if (loading) {
     return (
-        <Layout title="knowledge">
-            <div className="flex flex-col items-center h-screen overflow-y-auto">
-                <Navbar />
-                <div className={`${styles.dashboardOptions} grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center items-center my-5 mx-auto`}>
-                    <OptionCard number={1} title="BASIS" />
-                    <OptionCard number={2} title="ABAP" />
-                    <OptionCard number={3} title="FICO" />
-                    <OptionCard number={4} title="PM" />
-                    <OptionCard number={5} title="HCM" />
-                    <OptionCard number={6} title="SD" />
-                    <OptionCard number={7} title="MM" />
-                </div>
-            </div>
-        </Layout>
+      <Layout title="Loading">
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+          <div className="flex justify-center relative">
+            <div className="custom-loader"></div>
+          </div>
+        </div>
+      </Layout>
     );
+  }
+
+  return (
+    <Layout title="knowledge">
+      <div className="flex flex-col items-center h-screen overflow-y-auto">
+        <Navbar />
+        <div
+          className={`${styles.dashboardOptions} grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center items-center my-5 mx-auto`}
+        >
+          <OptionCard number={1} title="BASIS" user={user} />
+          <OptionCard number={2} title="ABAP" user={user} />
+          <OptionCard number={3} title="FICO" user={user} />
+          <OptionCard number={4} title="PM" user={user} />
+          <OptionCard number={5} title="HCM" user={user} />
+          <OptionCard number={6} title="SD" user={user} />
+          <OptionCard number={7} title="MM" user={user} />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 KnowledgeMedia.auth = true;
