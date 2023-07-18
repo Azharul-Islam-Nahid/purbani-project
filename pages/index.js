bb12479a09b995ed458b26c49325cb9e74c94ab4
Azharul Islam <lonewolfazharul@gmail.com>
2023-07-18 22:26:16 +0600

    preloader

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,29 @@ import MissionAndVision from "../components/landingPage/MissionAndVission";
 import OurValues from "../components/landingPage/OurValues";
 import Footer from "../components/landingPage/Footer";
 import Layout from "../components/common/Layout";
+import Preloader from "../components/preloader/preloader.js";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [loading, setLoading] = useState(true)
+
+  setTimeout(() => {
+
+    setLoading(false)
+
+  }, 2000);
+
+  if (loading) {
+
+
+    return (
+      <Preloader />
+    )
+
+  }
+
+
 
   return (
     <Layout title="Home">
